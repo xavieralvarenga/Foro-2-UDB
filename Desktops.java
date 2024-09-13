65d566bb966b19f7e7b7d489250994cad1dbd190
xavieralvarenga <xavierlarios10@gmail.com>
2024-09-13 07:52:33 -0600

    Carga de clase base y constructor Desktop

--- Desktops.java
+++ Desktops.java
@@ -1,3 +1,14 @@
-public class Desktops {
+public class Desktops extends Equipo{
+    private int RAM; //Memoria RAM
+    private String Grafica;
+    private float sizeCase; //Tamaño de la torre
+
+    public Desktops(String fabricante, String modelo, String Procesador, String hardisk, float pantalla, int ram, String grafica, float sizecase) {
+        super(fabricante, modelo, Procesador, hardisk, pantalla);
+        this.RAM = RAM;
+        this.Grafica = grafica;
+        this.sizeCase = sizecase;
+    }
     //Subclase
+
 }
--- Equipo.java
+++ Equipo.java
@@ -1,3 +1,16 @@
 public class Equipo {
     //Clase base para aplicar Herencia
+    protected String Fabricante;
+    protected String Modelo;
+    protected String Procesador;
+    protected String Hardisk;
+    protected float Pantalla; //Indica el tamaño de la pantalla
+
+    public Equipo(String fabricante, String modelo, String Procesador, String hardisk, float pantalla){
+        this.Fabricante = fabricante;
+        this.Modelo = modelo;
+        this.Procesador = Procesador;
+        this.Hardisk = hardisk;
+        this.Pantalla = pantalla;
+    }
 }
