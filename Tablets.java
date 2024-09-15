e92b4363b03a3e93c4caa0a12f1902487cb31dc4
Cristian2373 <ca928286@gmail.com>
2024-09-15 13:21:58 -0600

    se le agregego informacion a la sub clase tablets

--- Tablets.java
+++ Tablets.java
@@ -1,3 +1,18 @@
-public class Tablets {
-    //Subclase
+// Subclase Tablet
+class Tablet extends Equipo {
+    private String tipoPantalla;
+    private String sistemaOperativo;
+
+    public Tablet(String fabricante, String modelo, String Procesador, String hardisk, float pantalla,
+                  String tipoPantalla, String sistemaOperativo) {
+        super(fabricante, modelo, Procesador, hardisk, pantalla);
+        this.tipoPantalla = tipoPantalla;
+        this.sistemaOperativo = sistemaOperativo;
+    }
+
+    @Override
+    public String getDetalles() {
+        return super.getDetalles() + "\nTipo de Pantalla: " + tipoPantalla +
+                "\nSistema Operativo: " + sistemaOperativo;
+    }
 }
