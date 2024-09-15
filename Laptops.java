0305adc7870305abb914477c564150fb333d3991
Cristian2373 <ca928286@gmail.com>
2024-09-15 13:37:29 -0600

    se le agrego informacion a la sub clase laptops

--- Laptops.java
+++ Laptops.java
@@ -1,3 +1,15 @@
-public class Laptops {
-    //Subclase
+// Subclase Laptop
+class Laptop extends Equipo {
+    private String memoria;
+
+    public Laptop(String fabricante, String modelo, String Procesador, String hardisk, float pantalla,
+                  String memoria) {
+        super(fabricante, modelo, Procesador, hardisk, pantalla);
+        this.memoria = memoria;
+    }
+
+    @Override
+    public String getDetalles() {
+        return super.getDetalles() + "\nMemoria: " + memoria;
+    }
 }
